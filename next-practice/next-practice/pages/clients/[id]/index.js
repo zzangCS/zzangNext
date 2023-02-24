eef7bb16a157f7f465a7188319300051c293c5eb
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-24 19:23:25 +0900

    router

--- next-practice/next-practice/pages/clients/[id]/index.js
+++ next-practice/next-practice/pages/clients/[id]/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
 const ClientProjectsPage = () => {
+  const router = useRouter();
+  function loadProjectHandler() {
+    //load data
+    router.replace({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
   return (
-    <div>ClientProjectsPage</div>
-  )
-}
+    <>
+      <div>ClientProjectsPage</div>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </>
+  );
+};
 
-export default ClientProjectsPage
+export default ClientProjectsPage;
