e732c1831ba292178faeeba14120f3298e9f1f50
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-25 00:43:51 +0900

    section4: routing, styling

---
+++ next-practice/next-practice/components/events/event-item.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "next/link";
+
+import classes from "./event-item.module.css";
+
+const EventItem = (props) => {
+  const { title, image, date, location, id } = props;
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <>
+      <li className={classes.item}>
+        <img src={"/" + image} alt={title} />
+        <div className={classes.content}>
+          <div className={classes.summary}>
+            <h2>{title}</h2>
+            <div className={classes.date}>
+              <time>{humanReadableDate}</time>
+            </div>
+            <div className={classes.address}>
+              <address>{formattedAddress}</address>
+            </div>
+          </div>
+          <div className={classes.actions}>
+            <Link href={exploreLink}>Explore Event</Link>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+};
+
+export default EventItem;
---
+++ next-practice/next-practice/pages/events/[...slug].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilterEventsPage = () => {
+  return (
+    <div>FilterEventsPage</div>
+  )
+}
+
+export default FilterEventsPage
