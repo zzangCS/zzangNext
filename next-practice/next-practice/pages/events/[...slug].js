81d32a9cab50cbf1a99d8c5f829e6644277dd5ca
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-25 17:03:01 +0900

    [section4] filter all

--- next-practice/next-practice/pages/events/[...slug].js
+++ next-practice/next-practice/pages/events/[...slug].js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import EventList from "@/components/events/event-list";
+import { getFilteredEvents } from "@/dummy-data";
+import ResultsTitle from "@/components/events/results-title";
 
 const FilterEventsPage = () => {
-  return (
-    <div>FilterEventsPage</div>
-  )
-}
+  const router = useRouter();
+  const filterData = router.query.slug;
 
-export default FilterEventsPage
+  if (!filterData) {
+    return <p className="center">Loading..</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  // 데이터를 숫자로 변형
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
+  );
+};
+
+export default FilterEventsPage;
--- next-practice/next-practice/pages/events/index.js
+++ next-practice/next-practice/pages/events/index.js
@@ -1,13 +1,21 @@
 import { getAllEvents } from "@/dummy-data";
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
+import { useRouter } from "next/router";
 
 const AllEventsPage = () => {
   const events = getAllEvents(); //전체 이벤트 가져오기
+  const router = useRouter();
+
+  function findEventsHandler(yearn, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
