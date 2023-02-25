d533480822c628e3bf36900623a110bb4a750dfd
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-25 20:09:56 +0900

    [section5] context

--- next-practice/serverside/pages/index.js
+++ next-practice/serverside/pages/index.js
@@ -1,5 +1,5 @@
-import fs from 'fs/promises';
-import path from 'path';
+import fs from "fs/promises";
+import path from "path";
 
 function HomePage(props) {
   const { products } = props; //props객체의 products 키를 가져올 수 있음
@@ -12,15 +12,17 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   // process는 노드의 전역객체
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  console.log("Re generation");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData); //js객체로 파싱
   return {
     props: {
-      products: data.products
+      products: data.products,
     },
+    revalidate: 10, //초 단위: 재생성될 시간
   };
 }
 
