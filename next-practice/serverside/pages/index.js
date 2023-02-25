937a6705446fcf769d78d35fb3d88c2c3995bebe
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-25 18:09:14 +0900

    [section5] getStaticProps

--- next-practice/serverside/pages/index.js
+++ next-practice/serverside/pages/index.js
@@ -1,11 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 function HomePage(props) {
+  const { products } = props; //props객체의 products 키를 가져올 수 있음
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  // process는 노드의 전역객체
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData); //js객체로 파싱
+  return {
+    props: {
+      products: data.products
+    },
+  };
 }
 
 export default HomePage;
