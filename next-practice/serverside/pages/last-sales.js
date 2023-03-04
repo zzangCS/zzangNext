a20b9783dfc02b8075b4630c936e4cbdea23c9a9
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-03-04 15:26:37 +0900

    [section5] done

--- next-practice/serverside/pages/last-sales.js
+++ next-practice/serverside/pages/last-sales.js
@@ -1,50 +1,94 @@
 import { useEffect, useState } from "react/cjs/react.production.min";
 import useSWR from "swr";
 
-const LastSales = () => {
-  const [sales, setSales] = useState();
+const LastSales = (props) => {
+  const [sales, setSales] = useState(props.sales);
   // 로딩 fallback
-  const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
 
   const { data, error } = useSWR(
     "https://next-project-f8b87-default-rtdb.firebaseio.com/sales.json"
   );
 
-//   useEffect(() => {
-//     setIsLoading(true);
-//     fetch("https://next-project-f8b87-default-rtdb.firebaseio.com/sales.json")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const transformedSales = [];
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
 
-//         for (const key in data) {
-//           transformedSales.push({
-//             id: key,
-//             username: data[key].username,
-//             volume: data[key].volume,
-//           });
-//         }
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-//         setSales(transformedSales);
-//         setIsLoading(false);
-//       });
-//   }, []);
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("https://next-project-f8b87-default-rtdb.firebaseio.com/sales.json")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+  if (error) {
+    return <p>Failed to load...</p>;
+  }
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
-
   return (
     <ul>
       {sales.map((sale) => (
-        <li key={sales.id}></li>
+        <li key={sale.id}>
+          {sale.username}- ${sale.volume}
+        </li>
       ))}
     </ul>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch(
+    //프로미스 체인 반환
+    "https://next-project-f8b87-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  return {
+    props: {
+      sales: transformedSales,
+    },
+    revalidate: 10,
+  };
+}
 
 export default LastSales;
