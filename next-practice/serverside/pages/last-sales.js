20c2df10bd575b7f914dd57f04c3e953bcedc750
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-03-04 13:18:34 +0900

    [section5] client side data fetching

--- next-practice/serverside/pages/last-sales.js
+++ next-practice/serverside/pages/last-sales.js
@@ -1,14 +1,50 @@
-import { useEffect } from "react/cjs/react.production.min"
+import { useEffect, useState } from "react/cjs/react.production.min";
+import useSWR from "swr";
 
 const LastSales = () => {
-    useEffect(()=>{
-        fetch();
-    },[])
+  const [sales, setSales] = useState();
+  // 로딩 fallback
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://next-project-f8b87-default-rtdb.firebaseio.com/sales.json"
+  );
+
+//   useEffect(() => {
+//     setIsLoading(true);
+//     fetch("https://next-project-f8b87-default-rtdb.firebaseio.com/sales.json")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const transformedSales = [];
+
+//         for (const key in data) {
+//           transformedSales.push({
+//             id: key,
+//             username: data[key].username,
+//             volume: data[key].volume,
+//           });
+//         }
+
+//         setSales(transformedSales);
+//         setIsLoading(false);
+//       });
+//   }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!sales) {
+    return <p>No data yet</p>;
+  }
+
   return (
     <ul>
-        
-    </ul>>
-  )
-}
+      {sales.map((sale) => (
+        <li key={sales.id}></li>
+      ))}
+    </ul>
+  );
+};
 
-export default LastSales
+export default LastSales;
